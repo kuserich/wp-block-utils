9a7884e3b1b6c6d7e46ebcd47b73b573bce778b6
kuserich <markus@goeckeritz.ch>
2021-03-27 12:50:06 +0100

    updated npmignore, linted code, version bump 0.3.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,59 +1,59 @@
 import { babel } from '@rollup/plugin-babel';
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import filesize from "rollup-plugin-filesize";
-import localResolve from "rollup-plugin-local-resolve";
-import { terser } from "rollup-plugin-terser";
-import replace from "@rollup/plugin-replace";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import localResolve from 'rollup-plugin-local-resolve';
+import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
 const globals = {
-    "lodash-es": "lodash-es",
-    "rename-keys": "index",
-    classnames: "classnames",
+	'lodash-es': 'lodash-es',
+	'rename-keys': 'index',
+	classnames: 'classnames',
 };
 
 const config = {
-    input: "src/index.js",
-    output: [
-        {
-            file: pkg.browser,
-            format: "umd",
-            name: "BlockUtils",
-            globals
-        },
-        {
-            file: "dist/wp-block-utils.js",
-            format: "umd",
-            name: "BlockUtils",
-            globals
-        },
-        {
-            file: pkg.main,
-            format: "cjs",
-            name: "BlockUtils"
-        },
-        {
-            file: pkg.module,
-            format: "es"
-        }
-    ],
-    plugins: [
-        nodeResolve({
-            mainFields: ["module"],
-            extensions: [".js", ".jsx"]
-        }),
-        babel({ babelHelpers: 'bundled' }),
-        localResolve(),
-        commonjs(),
-        filesize(),
-        terser(),
-        replace({
-            "process.env.NODE_ENV": JSON.stringify("production")
-        })
-    ],
-    external: Object.keys(pkg.dependencies)
+	input: 'src/index.js',
+	output: [
+		{
+			file: pkg.browser,
+			format: 'umd',
+			name: 'BlockUtils',
+			globals,
+		},
+		{
+			file: 'dist/wp-block-utils.js',
+			format: 'umd',
+			name: 'BlockUtils',
+			globals,
+		},
+		{
+			file: pkg.main,
+			format: 'cjs',
+			name: 'BlockUtils',
+		},
+		{
+			file: pkg.module,
+			format: 'es',
+		},
+	],
+	plugins: [
+		nodeResolve( {
+			mainFields: [ 'module' ],
+			extensions: [ '.js', '.jsx' ],
+		} ),
+		babel( { babelHelpers: 'bundled' } ),
+		localResolve(),
+		commonjs(),
+		filesize(),
+		terser(),
+		replace( {
+			'process.env.NODE_ENV': JSON.stringify( 'production' ),
+		} ),
+	],
+	external: Object.keys( pkg.dependencies ),
 };
 
 export default config;
