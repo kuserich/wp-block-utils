5f87a5d069961c39a0bfb6a55765ffbf9af6a9d8
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-03-31 12:30:13 +0300

    Print-out editor specific inline CSS styles

--- src/components.js
+++ src/components.js
@@ -1,13 +1,20 @@
 /**
+ * External dependencies
+ */
+import { compact, forEach, map } from 'lodash-es';
+
+/**
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
 const { Spinner } = wp.components;
+const { useEffect } = wp.element;
+const { transformStyles } = wp.blockEditor;
 
 /**
  * A simple component to notify users that their action is being processed.
  *
- * @param {string} label Label shown above spinner.
+ * @param {string} label 	    Label shown above spinner.
  */
 const LoadingSpinner = ( { label } ) => (
 	<>
@@ -18,4 +25,28 @@ const LoadingSpinner = ( { label } ) => (
 	</>
 );
 
-export { LoadingSpinner };
+/**
+ * Applies a series of CSS rule transforms to wrap selectors inside a given class.
+ *
+ * @param  {Object} props 	            Block name/slug/id.
+ * @param  {Array}  props.styles 	    CSS rules.
+ */
+export default function EditorStyles( { styles } ) {
+	useEffect( () => {
+		const updatedStyles = transformStyles( styles, '.editor-styles-wrapper' );
+
+		const nodes = map( compact( updatedStyles ), ( updatedCSS ) => {
+			const node = document.createElement( 'style' );
+			node.innerHTML = updatedCSS;
+			document.body.appendChild( node );
+
+			return node;
+		} );
+
+		return () => forEach( nodes, ( node ) => document.body.removeChild( node ) );
+	}, [ styles ] );
+
+	return null;
+}
+
+export { LoadingSpinner, EditorStyles };
