423d652d8f5a3f278c60f8516bd392571aa49052
Sajedeh Gooklani <gookalani@mail.com>
2021-04-06 14:21:24 +0300

    Added the namespace argument

--- src/generator.js
+++ src/generator.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { snakeCase, forOwn, toLower } from 'lodash-es';
+import { snakeCase, forOwn, toLower, isUndefined } from 'lodash-es';
 import { PREFIX } from './constants';
 
 /**
@@ -9,10 +9,12 @@ import { PREFIX } from './constants';
  *
  * @param  {string} tag 	    Shortcode tag name.
  * @param  {Object} attrs 	    Shortcode attributes.
+ * @param  {string} namespace 	Project-specific prefix.
  * @return {string} 			Generated shortcode tag.
  */
-const generateShortcode = ( tag, attrs ) => {
-	let shortcode = `[${ snakeCase( PREFIX ) }_${ tag }`;
+const generateShortcode = ( tag, attrs, namespace ) => {
+	namespace = isUndefined( namespace ) ? PREFIX : namespace;
+	let shortcode = `[${ snakeCase( namespace ) }_${ tag }`;
 	forOwn( attrs, ( value, key ) => {
 		shortcode += ' ' + toLower( key ) + '="' + value + '"';
 	} );
