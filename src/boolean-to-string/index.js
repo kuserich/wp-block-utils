fae399782a00c95a505bc88d704c07e437b50b7b
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-20 21:51:59 +0300

    Converts a bool to a 'yes' or 'no'

---
+++ src/boolean-to-string/index.js
@@ -0,0 +1,38 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { isBoolean, isEqual } from 'lodash';
+
+/**
+ * Convert a string (e.g. 'yes' or 'no') to a bool.
+ *
+ * @ignore
+ */
+import stringToBoolean from '../string-to-boolean';
+
+/**
+ * Converts a bool to a 'yes' or 'no'.
+ *
+ * @function
+ * @since 	 	1.0.0
+ * @author	 	Mahdi Yazdani
+ * @copyright	sixa AG
+ * @param  	 	{boolean|string} input           Bool to convert. If a string is passed it will first be converted to a bool.
+ * @return 	 	{string}                         Converted value.
+ * @example
+ *
+ * booleanToString( true );
+ *
+ * // => boolean 'yes'
+ */
+const booleanToString = ( input ) => {
+	if ( ! isBoolean( input ) ) {
+		return stringToBoolean( input );
+	}
+
+	return isEqual( true, input ) ? 'yes' : 'no';
+};
+
+export default booleanToString;
