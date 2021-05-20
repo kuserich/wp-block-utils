7e3b24cba17e99362cee214792e09da10b49b4c3
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-20 22:32:11 +0300

    Insert an item into an array at a specific index

---
+++ src/insert-at-index/index.js
@@ -0,0 +1,27 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { concat, slice, add } from 'lodash';
+
+/**
+ * Insert an item into an array at a specific index.
+ *
+ * @function
+ * @since 	 	1.0.0
+ * @author	 	Mahdi Yazdani
+ * @copyright	sixa AG
+ * @param  	 	{Array}  input          The array to add to.
+ * @param  	 	{any}    value          The value to insert.
+ * @param  	 	{number} index          Given array index to insert the value.
+ * @return 	 	{Array}                 Returns the new updated array.
+ * @example
+ *
+ * insertAtIndex( [ 'a', 'd', 'c' ], 'b', 1 );
+ *
+ * // => Array [ 'a', 'b', 'c' ]
+ */
+const insertAtIndex = ( input, value, index ) => concat( slice( input, 0, index ), value, slice( input, add( index, 1 ) ) );
+
+export default insertAtIndex;
