11a80bf59269b4d48168002c83fa73bb9ceb7bb3
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-20 23:16:18 +0300

    Generate content-position CSS class name

---
+++ src/position-to-class-name/index.js
@@ -0,0 +1,39 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { get } from 'lodash';
+
+/**
+ * Determine whether the content is center positioned.
+ *
+ * @ignore
+ */
+import isPositionCenter from './../is-position-center';
+
+/**
+ * Possible background position values.
+ *
+ * @ignore
+ */
+import positions from './positions';
+
+/**
+ * Generates content-position CSS class name based on provided value.
+ *
+ * @function
+ * @since 	 	1.0.0
+ * @author	 	Mahdi Yazdani
+ * @copyright	sixa AG
+ * @param  	 	{string} value            CSS position value.
+ * @return 	 	{string}                  Corresponding CSS class name based on the provided position.
+ * @example
+ *
+ * positionToClassName( 'center right' );
+ *
+ * // => string 'is-position-center-right'
+ */
+const positionToClassName = ( value ) => ( isPositionCenter( value ) ? '' : get( positions, value ) );
+
+export default positionToClassName;
---
+++ src/position-to-class-name/positions.js
@@ -0,0 +1,27 @@
+/**
+ * Positions.
+ *
+ * Defines the positions used in adjusting background image positioning
+ * all hardcoded `position` values are based on the possible value of
+ * this CSS property from MDN docs.
+ *
+ * @constant
+ * @since 	 	1.0.0
+ * @author	 	Mahdi Yazdani
+ * @copyright	sixa AG
+ * @type        {Object}
+ * @see         https://developer.mozilla.org/en-US/docs/Web/CSS/background-position
+ * @ignore
+ */
+export default {
+	'top left': 'is-position-top-left',
+	'top center': 'is-position-top-center',
+	'top right': 'is-position-top-right',
+	'center left': 'is-position-center-left',
+	'center center': 'is-position-center-center',
+	center: 'is-position-center-center',
+	'center right': 'is-position-center-right',
+	'bottom left': 'is-position-bottom-left',
+	'bottom center': 'is-position-bottom-center',
+	'bottom right': 'is-position-bottom-right',
+};
