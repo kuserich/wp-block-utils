81e4a2dc35b10e4d59629773b7ae537b384c0165
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-20 20:42:52 +0300

    Create index.js

---
+++ src/background-image-style/index.js
@@ -0,0 +1,16 @@
+/**
+ * Generate inline background-image CSS style based on provided URL.
+ *
+ * @function
+ * @since 	 	1.0.0
+ * @author	 	Mahdi Yazdani
+ * @copyright	sixa AG
+ * @param  	 	{string} url         Background image URL or web address.
+ * @return 	 	{Object}             CSS style for the provided background image address.
+ * @example
+ *
+ * backgroundImageStyle( 'https://interactive-examples.mdn.mozilla.net/media/examples/lizard.png' );
+ */
+const backgroundImageStyle = ( url ) => ( url ? { backgroundImage: `url(${ url })` } : {} );
+
+export default backgroundImageStyle;
