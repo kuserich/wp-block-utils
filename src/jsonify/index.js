51dc87984ca7b01c763ef2911cdd9fe8d11f3111
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-20 22:53:02 +0300

    Parse a JSON string

---
+++ src/jsonify/index.js
@@ -0,0 +1,25 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { attempt } from 'lodash';
+
+/**
+ * Parses a JSON string, constructing the JavaScript value or object described by the string.
+ *
+ * @function
+ * @since 	 	1.0.0
+ * @author	 	Mahdi Yazdani
+ * @copyright	sixa AG
+ * @param  	 	{string} input            The string to parse as JSON.
+ * @return 	 	{Object}                  Converted value to object.
+ * @example
+ *
+ * jsonify( '[{"id":1,"title":"sunt aut facere"},{"id":2,"title":"qui est esse"}]' );
+ *
+ * // => Object [ { id: 1, title: 'sunt aut facere' }, { id: 2, title: 'qui est esse' } ]
+ */
+const jsonify = ( input ) => attempt( JSON.parse, input );
+
+export default jsonify;
