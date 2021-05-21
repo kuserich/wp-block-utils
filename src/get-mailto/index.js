4e404b190e304ec87b0115ad1c8e98c7a26c4468
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-21 23:03:29 +0300

    Extract email addresses from `mailto` href link

---
+++ src/get-mailto/index.js
@@ -0,0 +1,25 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { trim } from 'lodash';
+
+/**
+ * Extracts email addresses from `mailto` href link.
+ *
+ * @function
+ * @since 	 	1.0.0
+ * @author	 	Mahdi Yazdani
+ * @copyright	sixa AG
+ * @param  	 	{string} mailto           The href attribute value.
+ * @return 	 	{Array}                   Extracted email-addresses from the provided href value.
+ * @example
+ *
+ * getMailTo( 'mailto:info@example.com,info@test.com' );
+ *
+ * // => Array '[ 'info@example.com', 'info@test.com' ]'
+ */
+const getMailTo = ( mailto ) => trim( mailto ).match( /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi );
+
+export default getMailTo;
