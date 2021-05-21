983c43fdcbfdf7a7291545b832c13df41fb31a0b
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-21 11:45:35 +0300

    Return an object only with given path endings

---
+++ src/deep-pick/index.js
@@ -0,0 +1,30 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { forEach, split, get, set, first } from 'lodash';
+
+/**
+ * Creates an object composed of the (nested) picked object properties.
+ *
+ * @function
+ * @since 	 	1.0.0
+ * @author	 	Mahdi Yazdani
+ * @copyright	sixa AG
+ * @param  	 	{Object} collection           The source object.
+ * @param  	 	{Array}  paths                The property paths to pick.
+ * @return 	 	{Object}                      Returns the new object.
+ * @example
+ *
+ * deepPick( { id: 1, title: { rendered: 'Hello world!' } }, [ 'title.rendered' ] );
+ *
+ * // => Object { title: 'Hello world!' }
+ */
+const deepPick = ( collection, paths ) => {
+	const picked = {};
+	forEach( paths, ( path ) => set( picked, first( split( path, '.' ) ), get( collection, path ) ) );
+	return picked;
+};
+
+export default deepPick;
