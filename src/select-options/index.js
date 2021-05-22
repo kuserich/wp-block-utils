b8a5ed8bc56be1408f16eec905eef0b9f56bdc89
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-22 11:42:36 +0300

    Ability to prepend an empty option to the options

--- src/select-options/index.js
+++ src/select-options/index.js
@@ -3,7 +3,7 @@
  *
  * @ignore
  */
-import { map, mapKeys, first, split, keys, get } from 'lodash';
+import { map, mapKeys, split, get, first, keys, concat } from 'lodash';
 
 /**
  * Modify the names of the own enumerable properties (keys) of an object.
@@ -21,6 +21,13 @@ import renameKeys from 'rename-keys';
 import deepPick from './deep-pick';
 
 /**
+ * Text to display for showing no options being selected.
+ *
+ * @ignore
+ */
+import optionNone from './option-none';
+
+/**
  * Generates an array of objects to be passed to the `SelectControl` component.
  * SelectControl allow users to select from a single-option menu. It functions as a wrapper around the browser’s native `<select>` element.
  *
@@ -28,25 +35,29 @@ import deepPick from './deep-pick';
  * @since 	 	1.0.0
  * @author	 	Mahdi Yazdani
  * @copyright	sixa AG
- * @param  	 	{Array}  posts           The response on the API call of array of post objects.
- * @param  	 	{string} paths           The property paths to pick.
- * @return 	 	{Array}                  An array of objects containing the label to be shown to the user, and value used to choose the selected value.
+ * @param  	 	{Array}  posts          The response on the API call of array of post objects.
+ * @param  	 	{string} paths          The property paths to pick.
+ * @param  	 	{Array}  none        	Text to display for showing no options being selected.
+ * @return 	 	{Array}                 An array of objects containing the label to be shown to the user, and value used to choose the selected value.
  * @example
  *
  * selectOptions( [ { id: 1, title: { rendered: 'sunt aut facere' } }, { id: 2, title: { rendered: 'qui est esse' } } ], { id: 'value', 'title.rendered': 'label' } );
  *
  * // => Array [ { value: 1, label: 'Hello world!' }, { value: 2, label: 'qui est esse' } ]
  */
-const selectOptions = ( posts, paths ) =>
-	map(
-		map( posts, ( post ) => deepPick( post, keys( paths ) ) ),
-		( post ) =>
-			mapKeys( post, ( value, key ) =>
-				get(
-					renameKeys( paths, ( path ) => first( split( path, '.' ) ) ),
-					key
+const selectOptions = ( posts, paths, none = optionNone ) =>
+	concat(
+		none,
+		map(
+			map( posts, ( post ) => deepPick( post, keys( paths ) ) ),
+			( post ) =>
+				mapKeys( post, ( value, key ) =>
+					get(
+						renameKeys( paths, ( path ) => first( split( path, '.' ) ) ),
+						key
+					)
 				)
-			)
+		)
 	);
 
 export default selectOptions;
