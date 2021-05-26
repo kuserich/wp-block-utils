76163bd0e4b3ac1381a9375869dd281bdcc1556a
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-05-26 13:04:41 +0200

    Simplify test file, add further test wp-scripts, remove lint-staged step

--- __tests__/unit/string-to-boolean/index.test.js
+++ __tests__/unit/string-to-boolean/index.test.js
@@ -4,26 +4,6 @@
 import stringToBoolean from '../../../src/string-to-boolean';
 
 /**
- * Clearing all mocks to prevent carrying incorrect values between readings.
- */
-afterEach( () => {
-	jest.clearAllMocks();
-} );
-
-/**
- * Basic initial assertions (e.g. 'Does the function exist?', 'Is the function defined?').
- */
-describe( 'Initialization checks', () => {
-	test( 'stringToBoolean function exists', () => {
-		expect( stringToBoolean ).toBeDefined();
-	} );
-
-	test( 'stringToBoolean is a function', () => {
-		expect( typeof stringToBoolean ).toEqual( 'function' );
-	} );
-} );
-
-/**
  * Testing the function given one or a series of inputs against the expected return.
  */
 describe( "Convert given inputs to a 'true' boolean primitive.", () => {
