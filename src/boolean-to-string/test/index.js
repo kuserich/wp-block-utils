0f4018574d392abee446ddb5895e71079c195d8a
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-10 17:12:40 +0200

    -Amend- Update describe and separate cases more clearly

--- src/boolean-to-string/test/index.js
+++ src/boolean-to-string/test/index.js
@@ -9,6 +9,13 @@ describe( "Converts a boolean primitive to a 'yes' or 'no' string", () => {
 	test.each( [
 		[ true, 'yes' ],
 		[ false, 'no' ],
+	] )( 'when given %p it returns %p', ( input, expected ) => {
+		expect( booleanToString( input ) ).toBe( expected );
+	} );
+} );
+
+describe( "Converts other truthful values to boolean primitive 'true'", () => {
+	test.each( [
 		[ 'yes', true ],
 		[ 'true', true ],
 		[ 'TRUE', true ],
@@ -19,7 +26,7 @@ describe( "Converts a boolean primitive to a 'yes' or 'no' string", () => {
 	} );
 } );
 
-describe( "Converts any other input to a 'no' string", () => {
+describe( "Converts any other input to boolean primitive 'false'", () => {
 	test.each( [
 		[ '', false ],
 		[ '0', false ],
