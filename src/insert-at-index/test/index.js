c46a6db03d3cb32749d4deeb5653c7671bc7d6cd
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-11 19:45:28 +0200

    Add insert at index unit test

---
+++ src/insert-at-index/test/index.js
@@ -0,0 +1,65 @@
+/**
+ * The function to be tested.
+ *
+ * @ignore
+ */
+import insertAtIndex from '../';
+
+/**
+ * Example function used as replacement value.
+ *
+ * @return {string}          Returns example string.
+ * @ignore
+ */
+const exampleValueFunction = () => {
+	return 'b';
+};
+
+describe( 'Returns a valid array for a valid input with a valid value and index', () => {
+	test.each( [
+		[ [ 'a', 'd', 'c' ], 'b', 1, [ 'a', 'b', 'c' ] ],
+		[ [ 'a', 'b', 'c' ], '', 1, [ 'a', '', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], [ 'b' ], 1, [ 'a', 'b', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], [ [ 'b' ] ], 1, [ 'a', [ 'b' ], 'c' ] ],
+		[ [ 'a', 'd', 'c' ], { id: 'b' }, 1, [ 'a', { id: 'b' }, 'c' ] ],
+		[ [ 'a', 'd', 'c' ], exampleValueFunction(), 1, [ 'a', 'b', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', [ 1 ], [ 'a', 'b', 'c' ] ], // Array type index
+	] )( 'when given %p with value %p and index %p it returns %p', ( input, value, index, expected ) => {
+		expect( insertAtIndex( input, value, index ) ).toStrictEqual( expected );
+	} );
+} );
+
+describe( 'Returns a valid array for an invalid input with a valid value and index', () => {
+	test.each( [
+		[ [], 'a', 1, [ 'a' ] ],
+		[ {}, 'a', 1, [ 'a' ] ],
+		[ null, 'a', 1, [ 'a' ] ],
+		[ undefined, 'a', 1, [ 'a' ] ],
+	] )( 'when given %p with value %p and index %p it returns %p', ( input, value, index, expected ) => {
+		expect( insertAtIndex( input, value, index ) ).toStrictEqual( expected );
+	} );
+} );
+
+describe( 'Returns an invalid array for a valid input and value with invalid index', () => {
+	test.each( [
+		[ [ 'a', 'd', 'c' ], 'b', 5, [ 'a', 'd', 'c', 'b' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', -1, [ 'a', 'd', 'b', 'a', 'd', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', '1', [ 'a', 'b' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', '', [ 'b', 'd', 'c' ] ],
+	] )( 'when given %p with value %p and index %p it returns %p', ( input, value, index, expected ) => {
+		expect( insertAtIndex( input, value, index ) ).toStrictEqual( expected );
+	} );
+} );
+
+describe( 'Returns an invalid array for a valid input and value with falseful or empty index', () => {
+	test.each( [
+		[ [ 'a', 'd', 'c' ], 'b', [], [ 'b', 'd', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', {}, [ 'b', 'a', 'd', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', null, [ 'b', 'd', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', undefined, [ 'a', 'd', 'c', 'b', 'd', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', false, [ 'b', 'd', 'c' ] ],
+		[ [ 'a', 'd', 'c' ], 'b', true, [ 'a', 'b', 'c' ] ],
+	] )( 'when given %p with value %p and index %p it returns %p', ( input, value, index, expected ) => {
+		expect( insertAtIndex( input, value, index ) ).toStrictEqual( expected );
+	} );
+} );
