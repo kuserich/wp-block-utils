3246388e50ec824a8c87f9d6eae6bafb8c1d7934
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-11 19:57:30 +0200

    Refactor test structure and cases

--- src/is-non-empty-array/test/index.js
+++ src/is-non-empty-array/test/index.js
@@ -5,52 +5,34 @@
  */
 import isNonEmptyArray from '../';
 
-/**
- * Variations of valid array objects
- *
- * @ignore
- */
-const validInput = [
-	[ [ '1', 2, true ], true ],
-	[ [ '' ], true ],
-	[
+describe( "Return boolean primitive 'true' for a non-empty array object", () => {
+	test.each( [
+		[ [ '1', 2, true ], true ],
+		[ [ { id: 'a' } ], true ],
+		[ [ '' ], true ],
 		[
-			[ '1', '2' ],
-			[ '3', '4' ],
+			[
+				[ '1', '2' ],
+				[ '3', '4' ],
+			],
+			true,
 		],
-		true,
-	],
-];
+	] )( 'when given %p it returns %p', ( input, expected ) => {
+		expect( isNonEmptyArray( input ) ).toStrictEqual( expected );
+	} );
+} );
 
-/**
- * Variations of invalid input
- *
- * @ignore
- */
-const invalidInput = [
-	[ [], false ],
-	[ {}, false ],
-	[ 0, false ],
-	[ 1, false ],
-	[ '', false ],
-	[ false, false ],
-	[ null, false ],
-	[ undefined, false ],
-];
-
-describe( 'isNonEmptyArray', () => {
-	describe( 'Check if value passed is a non-empty array object', () => {
-		describe( 'Check if input is a non-empty array object', () => {
-			test.each( validInput )( 'when given %p it returns %p', ( input, expected ) => {
-				expect( isNonEmptyArray( input ) ).toStrictEqual( expected );
-				expect( input ).toBeArray();
-			} );
-		} );
-
-		describe( 'Check if any other input is a non-empty array object', () => {
-			test.each( invalidInput )( 'when given %p it returns %p', ( input, expected ) => {
-				expect( isNonEmptyArray( input ) ).toStrictEqual( expected );
-			} );
-		} );
+describe( "Return boolean primitive 'false' for empty or falseful values", () => {
+	test.each( [
+		[ [], false ],
+		[ {}, false ],
+		[ 0, false ],
+		[ 1, false ],
+		[ '', false ],
+		[ false, false ],
+		[ null, false ],
+		[ undefined, false ],
+	] )( 'when given %p it returns %p', ( input, expected ) => {
+		expect( isNonEmptyArray( input ) ).toStrictEqual( expected );
 	} );
 } );
