10556ffc31c266ece1edceff69989760fac37582
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-16 00:07:16 +0200

    Remove invalid cases, simplify and separate cases, fix describe

--- src/dim-ratio-class-name/test/index.js
+++ src/dim-ratio-class-name/test/index.js
@@ -5,26 +5,39 @@
  */
 import dimRatioClassName from '../';
 
-describe( 'Returns string with CSS class name for valid ratio or opacity number input', () => {
+describe( 'Returns string with valid CSS class name for valid ratio or opacity number input', () => {
 	test.each( [
-		[ 30, 'has-background-dim-30' ],
-		[ 65, 'has-background-dim-70' ],
-		[ 24.999, 'has-background-dim-20' ],
-		[ 7, 'has-background-dim-10' ],
 		[ 1, 'has-background-dim-0' ],
-		[ Number.MAX_SAFE_INTEGER, 'has-background-dim-9007199254740990' ],
-		[ -1, 'has-background-dim-0' ],
+		[ 7, 'has-background-dim-10' ],
+		[ 30, 'has-background-dim-30' ],
+		[ 100, 'has-background-dim-100' ],
 	] )( 'when given %p it returns %p', ( input, expected ) => {
 		expect( dimRatioClassName( input ) ).toBe( expected );
 	} );
 } );
-describe( 'Returns string with CSS class name for invalid ratio or opacity number input', () => {
+
+describe( 'Returns string with valid rounded CSS class name for valid ratio or opacity number input', () => {
 	test.each( [
-		[ [], 'has-background-dim-0' ],
-		[ [ '' ], 'has-background-dim-0' ],
-		[ {}, 'has-background-dim-NaN' ],
-		[ true, 'has-background-dim-0' ],
-		[ Number.MAX_VALUE, 'has-background-dim-1.7976931348623157e+308' ],
+		[ 65, 'has-background-dim-70' ],
+		[ 24.99, 'has-background-dim-20' ],
+	] )( 'when given %p it returns %p', ( input, expected ) => {
+		expect( dimRatioClassName( input ) ).toBe( expected );
+	} );
+} );
+
+describe( "Returns empty string for valid input number '0' or '50'", () => {
+	test.each( [
+		[ 0, '' ],
+		[ 50, '' ],
+	] )( 'when given %p it returns %p', ( input, expected ) => {
+		expect( dimRatioClassName( input ) ).toBe( expected );
+	} );
+} );
+
+describe( 'Returns invalid string for unbounded ratio or opacity number input', () => {
+	test.each( [
+		[ Number.MAX_SAFE_INTEGER, 'has-background-dim-9007199254740990' ],
+		[ -1, 'has-background-dim-0' ],
 	] )( 'when given %p it returns %p', ( input, expected ) => {
 		expect( dimRatioClassName( input ) ).toBe( expected );
 	} );
@@ -33,8 +46,6 @@ describe( 'Returns string with CSS class name for invalid ratio or opacity numbe
 describe( 'Returns empty string for falseful and empty values', () => {
 	test.each( [
 		[ '', '' ],
-		[ 0, '' ],
-		[ 50, '' ],
 		[ null, '' ],
 		[ false, '' ],
 		[ undefined, '' ],
