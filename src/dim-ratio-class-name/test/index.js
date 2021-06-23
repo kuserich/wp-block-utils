af23f15221e1409cd9c2ab50947d7433d3335b40
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-23 14:21:51 +0200

    Update describe pattern, alter quote styles and placeholders, added falsely cases

--- src/dim-ratio-class-name/test/index.js
+++ src/dim-ratio-class-name/test/index.js
@@ -5,50 +5,53 @@
  */
 import dimRatioClassName from '../';
 
-describe( 'Returns string with valid CSS class name for valid ratio or opacity number input', () => {
+describe( 'Should return string with valid CSS class name for valid ratio or opacity number input', () => {
 	test.each( [
 		[ 1, 'has-background-dim-0' ],
 		[ 7, 'has-background-dim-10' ],
 		[ 30, 'has-background-dim-30' ],
 		[ 100, 'has-background-dim-100' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %i it returns %s', ( input, expected ) => {
 		expect( dimRatioClassName( input ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Returns string with valid rounded CSS class name for valid ratio or opacity number input', () => {
+describe( 'Should return string with valid rounded CSS class name for valid ratio or opacity number input', () => {
 	test.each( [
 		[ 65, 'has-background-dim-70' ],
 		[ 24.99, 'has-background-dim-20' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %d it returns %s', ( input, expected ) => {
 		expect( dimRatioClassName( input ) ).toBe( expected );
 	} );
 } );
 
-describe( "Returns empty string for valid input number '0' or '50'", () => {
+describe( 'Should return empty string for valid input number "0" or "50"', () => {
 	test.each( [
 		[ 0, '' ],
 		[ 50, '' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %i it returns %p', ( input, expected ) => {
 		expect( dimRatioClassName( input ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Returns invalid string for unbounded ratio or opacity number input', () => {
+describe( 'Should return invalid string for unbounded ratio or opacity number input', () => {
 	test.each( [
 		[ Number.MAX_SAFE_INTEGER, 'has-background-dim-9007199254740990' ],
 		[ -1, 'has-background-dim-0' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %i it returns %s', ( input, expected ) => {
 		expect( dimRatioClassName( input ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Returns empty string for falseful and empty values', () => {
+describe( 'Should return empty string for falseful and empty values', () => {
 	test.each( [
 		[ '', '' ],
 		[ null, '' ],
 		[ false, '' ],
 		[ undefined, '' ],
+		[ -0, '' ],
+		[ 0n, '' ],
+		[ NaN, '' ],
 	] )( 'when given %p it returns %p', ( input, expected ) => {
 		expect( dimRatioClassName( input ) ).toBe( expected );
 	} );
