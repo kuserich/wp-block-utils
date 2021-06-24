53a368789a765b2c354ca61d8967596160abe990
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-24 07:35:30 +0200

    Update describe pattern, use `toStrictEqual`, use `%s` placeholder

--- src/hex-to-emoji/test/index.js
+++ src/hex-to-emoji/test/index.js
@@ -12,47 +12,47 @@ import hexToEmoji from '../';
  */
 const removeInvalidUnicodeChars = /[\u0000-\u001F\u007F-\u009F\u200B-\u200D\u200C\u200E\u200F\uFEFF]/g;
 
-describe( 'Returns rendered Emoji icon for a hex-unicode', () => {
+describe( 'Should return rendered Emoji icon for a hex-unicode', () => {
 	test( 'should render single Emoji icon for a valid hex-unicode', () => {
-		expect( hexToEmoji( '1f603' ) ).toEqual( '😃' );
+		expect( hexToEmoji( '1f603' ) ).toStrictEqual( '😃' );
 	} );
 
 	test( 'should render multiple Emoji icons for multiple valid hex-unicodes', () => {
-		expect( hexToEmoji( '1f60a 1f603' ) ).toEqual( '😊😃' );
+		expect( hexToEmoji( '1f60a 1f603' ) ).toStrictEqual( '😊😃' );
 	} );
 } );
 
-describe( 'Returns invalid string for an untrimmed or multi-space separated hex-unicode', () => {
+describe( 'Should return invalid string for an untrimmed or multi-space separated hex-unicode', () => {
 	test( 'should return invalid string for an untrimmed hex-unicode', () => {
-		expect( hexToEmoji( ' 1f603 ' ) ).toEqual( '&#x😃&#x;' );
+		expect( hexToEmoji( ' 1f603 ' ) ).toStrictEqual( '&#x😃&#x;' );
 	} );
 
 	test( 'should return invalid string for multiple hex-unicodes multi-space separated', () => {
-		expect( hexToEmoji( '1f60a  1f603' ) ).toEqual( '😊&#x😃' );
+		expect( hexToEmoji( '1f60a  1f603' ) ).toStrictEqual( '😊&#x😃' );
 	} );
 } );
 
-describe( 'Returns invalid string for an invalid or non-existent hex-unicode', () => {
+describe( 'Should return invalid string for an invalid or non-existent hex-unicode', () => {
 	test( 'should return invalid string for a non-existent hex-unicode', () => {
-		expect( hexToEmoji( '1fz99' ).replace( removeInvalidUnicodeChars, '' ) ).toEqual( 'z99;' );
+		expect( hexToEmoji( '1fz99' ).replace( removeInvalidUnicodeChars, '' ) ).toStrictEqual( 'z99;' );
 	} );
 
 	test( 'should return invalid string for an invalid hex-unicode', () => {
-		expect( hexToEmoji( 'test123' ).replace( removeInvalidUnicodeChars, '' ) ).toEqual( '&#xtest123;' );
+		expect( hexToEmoji( 'test123' ).replace( removeInvalidUnicodeChars, '' ) ).toStrictEqual( '&#xtest123;' );
 	} );
 } );
 
-describe( 'Returns invalid string for falseful or empty value', () => {
+describe( 'Should return invalid string for falseful or empty value', () => {
 	test.each( [
 		[ [], '&#x;' ],
 		[ {}, '&#x[object&#xobject];' ],
 		[ 0, '�' ], // Returns replacement character, which replaces an invalid or unrecognizable character.
 		[ 1, '' ],
 		[ '', '&#x;' ],
-		[ false, 'úlse;' ], // Returns unicode character 'ú', UTF-16 encoded as 0x00FA.
+		[ false, 'úlse;' ], // Returns unicode character `ú`, UTF-16 encoded as 0x00FA.
 		[ null, '&#x;' ],
 		[ undefined, '&#x;' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %p it returns %s', ( input, expected ) => {
 		expect( hexToEmoji( input ).replace( removeInvalidUnicodeChars, '' ) ).toStrictEqual( expected );
 	} );
 } );
