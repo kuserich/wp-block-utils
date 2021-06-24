4d864ead61393c6cd009e80884fe75489bec2784
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-24 07:55:15 +0200

    Update describe pattern, replace `test` with `it`, add falsely cases

--- src/remove-at-index/test/index.js
+++ src/remove-at-index/test/index.js
@@ -5,8 +5,8 @@
  */
 import removeAtIndex from '../';
 
-describe( 'Returns a valid array for a valid input array with a valid index', () => {
-	test.each( [
+describe( 'Should return a valid array for a valid input array with a valid index', () => {
+	it.each( [
 		[ [ 'a', 'd', 'b' ], 1, [ 'a', 'b' ] ],
 		[ [ 'a', [ 'd' ], 'b' ], 1, [ 'a', 'b' ] ],
 		[ [ 'a', 'd', 'b' ], [ 1 ], [ 'a', 'b' ] ],
@@ -16,30 +16,37 @@ describe( 'Returns a valid array for a valid input array with a valid index', ()
 	} );
 } );
 
-describe( 'Returns an invalid array for an invalid input array with a valid index', () => {
-	test.each( [
+describe( 'Should return an invalid array for an invalid input array with a valid index', () => {
+	it.each( [
 		[ [], 1, [] ],
 		[ {}, 1, [] ],
 		[ null, 1, [] ],
 		[ undefined, 1, [] ],
+		[ 0, 1, [] ],
+		[ -0, 1, [] ],
+		[ 0n, 1, [] ],
+		[ NaN, 1, [] ],
 	] )( 'when given %p with index %p it returns %p', ( input, index, expected ) => {
 		expect( removeAtIndex( input, index ) ).toStrictEqual( expected );
 	} );
 } );
 
-describe( 'Returns an invalid array for a valid input array with invalid index', () => {
-	test.each( [
+describe( 'Should return an invalid array for a valid input array with invalid index', () => {
+	it.each( [
 		[ [ 'a', 'd', 'b' ], 3, [ 'a', 'd', 'b' ] ],
 		[ [ 'a', 'd', 'b' ], -1, [ 'a', 'd', 'a', 'd', 'b' ] ],
 		[ [ 'a', 'd', 'b' ], '1', [ 'a' ] ],
 		[ [ 'a', 'd', 'b' ], '', [ 'd', 'b' ] ],
+		[ [ 'a', 'd', 'b' ], 0, [ 'd', 'b' ] ],
+		[ [ 'a', 'd', 'b' ], -0, [ 'd', 'b' ] ],
+		[ [ 'a', 'd', 'b' ], NaN, [ 'a', 'd', 'b' ] ],
 	] )( 'when given %p with index %p it returns %p', ( input, index, expected ) => {
 		expect( removeAtIndex( input, index ) ).toStrictEqual( expected );
 	} );
 } );
 
-describe( 'Returns an invalid array for a valid input array with falseful or empty index', () => {
-	test.each( [
+describe( 'Should return an invalid array for a valid input array with falseful or empty index', () => {
+	it.each( [
 		[ [ 'a', 'd', 'b' ], [], [ 'd', 'b' ] ],
 		[ [ 'a', 'd', 'b' ], {}, [ 'a', 'd', 'b' ] ],
 		[ [ 'a', 'd', 'b' ], null, [ 'd', 'b' ] ],
