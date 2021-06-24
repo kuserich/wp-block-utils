451dfe82b1fed255ab5ebea945f329f1ab837ac3
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-24 07:41:06 +0200

    Update describe pattern, use `toBe`, use `%s` placeholder, replace `test` with `it`, add falsely cases

--- src/position-to-class-name/test/index.js
+++ src/position-to-class-name/test/index.js
@@ -5,17 +5,17 @@
  */
 import positionToClassName from '../';
 
-describe( "Returns content-position CSS class name string for string 'center' or 'center center'", () => {
-	test.each( [
+describe( 'Should return content-position CSS class name string for string "center" or "center center"', () => {
+	it.each( [
 		[ 'center', '' ],
 		[ 'center center', '' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( positionToClassName( input ) ).toStrictEqual( expected );
+	] )( 'when given %s it returns %s', ( input, expected ) => {
+		expect( positionToClassName( input ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Returns content-position CSS class name string for other defined positions strings', () => {
-	test.each( [
+describe( 'Should return content-position CSS class name string for other defined positions strings', () => {
+	it.each( [
 		[ 'top left', 'is-position-top-left' ],
 		[ 'top center', 'is-position-top-center' ],
 		[ 'top right', 'is-position-top-right' ],
@@ -24,13 +24,13 @@ describe( 'Returns content-position CSS class name string for other defined posi
 		[ 'bottom left', 'is-position-bottom-left' ],
 		[ 'bottom center', 'is-position-bottom-center' ],
 		[ 'bottom right', 'is-position-bottom-right' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( positionToClassName( input ) ).toStrictEqual( expected );
+	] )( 'when given %s it returns %s', ( input, expected ) => {
+		expect( positionToClassName( input ) ).toBe( expected );
 	} );
 } );
 
-describe( "Returns primitive 'undefined' for invalid string and empty value", () => {
-	test.each( [
+describe( 'Should return primitive "undefined" for invalid string and empty value', () => {
+	it.each( [
 		[ 'Top left', undefined ],
 		[ 'Top  left', undefined ],
 		[ 'Top left right', undefined ],
@@ -38,19 +38,22 @@ describe( "Returns primitive 'undefined' for invalid string and empty value", ()
 		[ [], undefined ],
 		[ {}, undefined ],
 		[ true, undefined ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %s it returns %s', ( input, expected ) => {
 		expect( positionToClassName( input ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Returns empty string for falseful value', () => {
-	test.each( [
+describe( 'Should return empty string for falseful value', () => {
+	it.each( [
 		[ '', '' ],
 		[ null, '' ],
 		[ false, '' ],
 		[ undefined, '' ],
 		[ 0, '' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+		[ -0, '' ],
+		[ 0n, '' ],
+		[ NaN, '' ],
+	] )( 'when given %s it returns %s', ( input, expected ) => {
 		expect( positionToClassName( input ) ).toBe( expected );
 	} );
 } );
