84902c8fb5d861e0b170d68f5e7926f0d9e38822
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-24 09:48:59 +0200

    Update describe pattern, use `%o` placeholder, replace `test` with `it`, add falsely cases

--- src/shortcode/test/index.js
+++ src/shortcode/test/index.js
@@ -5,44 +5,47 @@
  */
 import shortcode from '../';
 
-describe( 'Returns a valid string shortcode for valid tag name and attributes', () => {
-	test.each( [
+describe( 'Should return a valid string shortcode for valid tag name and attributes', () => {
+	it.each( [
 		[ 'test_tag', { posts: 1 }, '[test_tag posts="1"]' ],
 		[ 'Test Tag', { posts: 1 }, '[test_tag posts="1"]' ],
 		[ 'test_tag', { posts: [ 1, 2 ] }, '[test_tag posts="1,2"]' ],
 		[ 'test_tag', { posts: '1,2' }, '[test_tag posts="1,2"]' ],
 		[ 'test_tag', { camelCase: 1 }, '[test_tag camel_case="1"]' ],
-	] )( 'when given %p with attributes %p it returns %p', ( tagName, attributes, expected ) => {
-		expect( shortcode( tagName, attributes ) ).toStrictEqual( expected );
+	] )( 'when given %s with attributes %o it returns %s', ( tagName, attributes, expected ) => {
+		expect( shortcode( tagName, attributes ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Returns an invalid string shortcode for an invalid tag name and valid attribute', () => {
-	test.each( [
+describe( 'Should return an invalid string shortcode for an invalid tag name and valid attribute', () => {
+	it.each( [
 		[ [ 'test_tag' ], { posts: 1 }, '[test_tag posts="1"]' ],
 		[ [ 'test_tag', 'another_tag' ], { posts: 1 }, '[test_tag_another_tag posts="1"]' ],
 		[ {}, { posts: 1 }, '[object_object posts="1"]' ],
 		[ false, { posts: 1 }, '[false posts="1"]' ],
 		[ true, { posts: 1 }, '[true posts="1"]' ],
-	] )( 'when given %p with attributes %p it returns %p', ( tagName, attributes, expected ) => {
-		expect( shortcode( tagName, attributes ) ).toStrictEqual( expected );
+	] )( 'when given %p with attributes %o it returns %s', ( tagName, attributes, expected ) => {
+		expect( shortcode( tagName, attributes ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Returns an invalid string shortcode for an empty or falseful tag name and valid attribute', () => {
-	test.each( [
+describe( 'Should return an invalid string shortcode for an empty or falseful tag name and valid attribute', () => {
+	it.each( [
 		[ [ '' ], { posts: 1 }, '[ posts="1"]' ],
 		[ [], { posts: 1 }, '[ posts="1"]' ],
 		[ '', { posts: 1 }, '[ posts="1"]' ],
 		[ null, { posts: 1 }, '[ posts="1"]' ],
 		[ undefined, { posts: 1 }, '[ posts="1"]' ],
-	] )( 'when given %p with attributes %p it returns %p', ( tagName, attributes, expected ) => {
-		expect( shortcode( tagName, attributes ) ).toStrictEqual( expected );
+		[ 0, { posts: 1 }, '[0 posts="1"]' ],
+		[ -0, { posts: 1 }, '[0 posts="1"]' ],
+		[ NaN, { posts: 1 }, '[na_n posts="1"]' ],
+	] )( 'when given %p with attributes %o it returns %s', ( tagName, attributes, expected ) => {
+		expect( shortcode( tagName, attributes ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Returns an invalid string shortcode for a valid tag name and empty or falseful attribute', () => {
-	test.each( [
+describe( 'Should return an invalid string shortcode for a valid tag name and empty or falseful attribute', () => {
+	it.each( [
 		[ 'test_tag', [ '' ], '[test_tag 0=""]' ],
 		[ 'test_tag', '', '[test_tag]' ],
 		[ 'test_tag', {}, '[test_tag]' ],
@@ -51,7 +54,11 @@ describe( 'Returns an invalid string shortcode for a valid tag name and empty or
 		[ 'test_tag', true, '[test_tag]' ],
 		[ 'test_tag', undefined, '[test_tag]' ],
 		[ 'test_tag', null, '[test_tag]' ],
-	] )( 'when given %p with attributes %p it returns %p', ( tagName, attributes, expected ) => {
-		expect( shortcode( tagName, attributes ) ).toStrictEqual( expected );
+		[ 'test_tag', 0, '[test_tag]' ],
+		[ 'test_tag', -0, '[test_tag]' ],
+		[ 'test_tag', 0n, '[test_tag]' ],
+		[ 'test_tag', NaN, '[test_tag]' ],
+	] )( 'when given %s with attributes %p it returns %s', ( tagName, attributes, expected ) => {
+		expect( shortcode( tagName, attributes ) ).toBe( expected );
 	} );
 } );
