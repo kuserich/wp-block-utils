cd75c6cbf4572f1b6eca7cc082bb3a0851a0a24c
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-25 10:20:32 +0200

    Remove invalid cases, update `describe` pattern

--- src/boolean-to-string/test/index.js
+++ src/boolean-to-string/test/index.js
@@ -5,35 +5,33 @@
  */
 import booleanToString from '../';
 
-describe( 'Converts a boolean primitive to a "yes" or "no" string', () => {
-	test.each( [
+describe( 'Should convert a boolean primitive to a "yes" or "no" string', () => {
+	it.each( [
 		[ true, 'yes' ],
 		[ false, 'no' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %p it returns %s', ( input, expected ) => {
 		expect( booleanToString( input ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Converts other truthful values to boolean primitive "true"', () => {
-	test.each( [
+describe( 'Should convert other truthful values to boolean primitive "true"', () => {
+	it.each( [
 		[ 'yes', true ],
 		[ 'true', true ],
 		[ 'TRUE', true ],
 		[ '1', true ],
-		[ 1, true ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %s it returns %p', ( input, expected ) => {
 		expect( booleanToString( input ) ).toBe( expected );
 	} );
 } );
 
-describe( 'Converts any other input to boolean primitive "false"', () => {
-	test.each( [
+describe( 'Should convert any other input to boolean primitive "false"', () => {
+	it.each( [
 		[ '', false ],
 		[ '0', false ],
 		[ 'no', false ],
 		[ 'false', false ],
 		[ 'FALSE', false ],
-		[ 0, false ],
 		[ 2, false ],
 		[ {}, false ],
 		[ [], false ],
