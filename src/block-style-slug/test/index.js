b038c5a822da3b66f6b7da32ba5a1a43f1b68cef
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-25 09:47:23 +0200

    Remove invalid cases, replace `test` with `it`

--- src/block-style-slug/test/index.js
+++ src/block-style-slug/test/index.js
@@ -6,11 +6,9 @@
 import blockStyleSlug from '../';
 
 describe( 'Should extract a valid block style name from a given string of CSS class name(s)', () => {
-	test.each( [
+	it.each( [
 		[ 'is-style-fancy', 'fancy' ],
 		[ 'is-style-fancy123', 'fancy123' ],
-		[ 'is-style-ALLCAPS', 'ALLCAPS' ],
-		[ 'is-style-special_char', 'special_char' ],
 		[ 'test-class-name is-style-fancy', 'fancy' ],
 		[ 'class-nameis-style-fancy', 'fancy' ],
 	] )( 'when given %s it returns %s', ( input, expected ) => {
@@ -19,7 +17,7 @@ describe( 'Should extract a valid block style name from a given string of CSS cl
 } );
 
 describe( 'Should return an empty string when none of the given string argument of CSS class name(s) includes block style class name', () => {
-	test.each( [
+	it.each( [
 		[ 'IS-style-fancy', '' ],
 		[ 'test-class-name', '' ],
 	] )( 'when given %s it returns %s', ( input, expected ) => {
@@ -28,7 +26,7 @@ describe( 'Should return an empty string when none of the given string argument 
 } );
 
 describe( 'Should return empty string for falseful and empty values', () => {
-	test.each( [
+	it.each( [
 		[ false, '' ],
 		[ true, '' ],
 		[ null, '' ],
