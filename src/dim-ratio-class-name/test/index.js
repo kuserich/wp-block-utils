a8776cc39dd4986fa84f8d7f4fbfbfa7fbad4638
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-06-25 19:09:46 +0200

    Update `describe`, replace `test` with `it`, remove invalid cases

--- src/dim-ratio-class-name/test/index.js
+++ src/dim-ratio-class-name/test/index.js
@@ -5,8 +5,8 @@
  */
 import dimRatioClassName from '../';
 
-describe( 'Should return string with valid CSS class name for valid ratio or opacity number input', () => {
-	test.each( [
+describe( 'Should return a string CSS class name given an integer dim ratio', () => {
+	it.each( [
 		[ 1, 'has-background-dim-0' ],
 		[ 7, 'has-background-dim-10' ],
 		[ 30, 'has-background-dim-30' ],
@@ -16,8 +16,8 @@ describe( 'Should return string with valid CSS class name for valid ratio or opa
 	} );
 } );
 
-describe( 'Should return string with valid rounded CSS class name for valid ratio or opacity number input', () => {
-	test.each( [
+describe( 'Should return a string CSS class name with rounded value given an integer or decimal dim ratio', () => {
+	it.each( [
 		[ 65, 'has-background-dim-70' ],
 		[ 24.99, 'has-background-dim-20' ],
 	] )( 'when given %d it returns %s', ( input, expected ) => {
@@ -25,8 +25,8 @@ describe( 'Should return string with valid rounded CSS class name for valid rati
 	} );
 } );
 
-describe( 'Should return empty string for valid input number "0" or "50"', () => {
-	test.each( [
+describe( 'Should return empty string given integer "0" or "50"', () => {
+	it.each( [
 		[ 0, '' ],
 		[ 50, '' ],
 	] )( 'when given %i it returns %p', ( input, expected ) => {
@@ -34,17 +34,8 @@ describe( 'Should return empty string for valid input number "0" or "50"', () =>
 	} );
 } );
 
-describe( 'Should return invalid string for unbounded ratio or opacity number input', () => {
-	test.each( [
-		[ Number.MAX_SAFE_INTEGER, 'has-background-dim-9007199254740990' ],
-		[ -1, 'has-background-dim-0' ],
-	] )( 'when given %i it returns %s', ( input, expected ) => {
-		expect( dimRatioClassName( input ) ).toBe( expected );
-	} );
-} );
-
 describe( 'Should return empty string for falseful and empty values', () => {
-	test.each( [
+	it.each( [
 		[ '', '' ],
 		[ null, '' ],
 		[ false, '' ],
@@ -52,7 +43,7 @@ describe( 'Should return empty string for falseful and empty values', () => {
 		[ -0, '' ],
 		[ 0n, '' ],
 		[ NaN, '' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
+	] )( 'when given %p it returns %s', ( input, expected ) => {
 		expect( dimRatioClassName( input ) ).toBe( expected );
 	} );
 } );
