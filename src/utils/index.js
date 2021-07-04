0edbc455a6ffdbe888333fb244a49d3d436792fb
Markus <markus.goeckeritz@sixa.ch>
2021-07-04 21:33:28 +0200

    added @ignore to utils constants

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,21 @@
 /**
  * Used to provide falsey values to methods.
+ *
+ * @ignore
  */
 const falsey = [ , null, undefined, false, 0, NaN, '' ];
 
 /**
  * Used to provide empty values to methods.
+ *
+ * @ignore
  */
 const empties = [ [], {}, null, undefined, false, 0, NaN, '' ];
 
 /**
  * Used to provide primitive values to methods.
+ *
+ * @ignore
  */
 const primitives = [ null, undefined, false, true, 1, NaN, 'a' ];
 
