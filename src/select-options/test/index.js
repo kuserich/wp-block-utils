47f246e0a0a06e6d2bc442f4d222ae91e3588f7a
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-07-06 11:27:40 +0200

    Use utils, update describes

--- src/select-options/test/index.js
+++ src/select-options/test/index.js
@@ -1,109 +1,127 @@
 /**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { map } from 'lodash';
+
+/**
  * The function to be tested.
  *
  * @ignore
  */
 import selectOptions from '../';
 
-describe( 'Should return array of objects for valid post objects array with valid property path and no custom "none" option value', () => {
-	it.each( [
-		[ [ { id: 1 }, { id: 2 } ], { id: 'value' }, [ { value: '', label: '— Select —' }, { value: 1 }, { value: 2 } ] ],
-		[
-			[
-				{ id: 1, title: 'sunt aut facere' },
-				{ id: 2, title: 'qui est esse' },
-			],
-			{ id: 'value', title: 'label' },
-			[
-				{ value: '', label: '— Select —' },
-				{ value: 1, label: 'sunt aut facere' },
-				{ value: 2, label: 'qui est esse' },
-			],
-		],
-		[
-			[
-				{ id: 1, title: { rendered: 'sunt aut facere' } },
-				{ id: 2, title: { rendered: 'qui est esse' } },
-			],
-			{ id: 'value', 'title.rendered': 'label' },
-			[
-				{ value: '', label: '— Select —' },
-				{ value: 1, label: 'sunt aut facere' },
-				{ value: 2, label: 'qui est esse' },
-			],
-		],
-	] )( 'when given %o with path %o it returns %o', ( input, paths, expected ) => {
-		expect( selectOptions( input, paths ) ).toStrictEqual( expected );
-	} );
-} );
+/**
+ * Set of falsey and empty values for testing.
+ *
+ * @ignore
+ */
+import { falsey, empties } from '../../utils';
 
-describe( 'Should return array of objects for valid post objects array with valid property path and empty "none" option value', () => {
-	it.each( [
-		[ [ { id: 1 }, { id: 2 } ], { id: 'value' }, { value: '', label: '' }, [ { value: '', label: '' }, { value: 1 }, { value: 2 } ] ],
-		[
+describe( 'selectOptions', () => {
+	describe( 'Should return array of objects for valid post objects array with valid property path and no custom "none" option value', () => {
+		it.each( [
+			[ [ { id: 1 }, { id: 2 } ], { id: 'value' }, [ { value: '', label: '— Select —' }, { value: 1 }, { value: 2 } ] ],
 			[
-				{ id: 1, title: { rendered: 'sunt aut facere' } },
-				{ id: 2, title: { rendered: 'qui est esse' } },
+				[
+					{ id: 1, title: 'sunt aut facere' },
+					{ id: 2, title: 'qui est esse' },
+				],
+				{ id: 'value', title: 'label' },
+				[
+					{ value: '', label: '— Select —' },
+					{ value: 1, label: 'sunt aut facere' },
+					{ value: 2, label: 'qui est esse' },
+				],
 			],
-			{ id: 'value', 'title.rendered': 'label' },
-			{ value: '', label: '' },
 			[
+				[
+					{ id: 1, title: { rendered: 'sunt aut facere' } },
+					{ id: 2, title: { rendered: 'qui est esse' } },
+				],
+				{ id: 'value', 'title.rendered': 'label' },
+				[
+					{ value: '', label: '— Select —' },
+					{ value: 1, label: 'sunt aut facere' },
+					{ value: 2, label: 'qui est esse' },
+				],
+			],
+		] )( 'when given %o with path %o it returns %o', ( input, paths, expected ) => {
+			expect( selectOptions( input, paths ) ).toStrictEqual( expected );
+		} );
+	} );
+
+	describe( 'Should return array of objects for valid post objects array with valid property path and empty "none" option value', () => {
+		it.each( [
+			[ [ { id: 1 }, { id: 2 } ], { id: 'value' }, { value: '', label: '' }, [ { value: '', label: '' }, { value: 1 }, { value: 2 } ] ],
+			[
+				[
+					{ id: 1, title: { rendered: 'sunt aut facere' } },
+					{ id: 2, title: { rendered: 'qui est esse' } },
+				],
+				{ id: 'value', 'title.rendered': 'label' },
 				{ value: '', label: '' },
-				{ value: 1, label: 'sunt aut facere' },
-				{ value: 2, label: 'qui est esse' },
+				[
+					{ value: '', label: '' },
+					{ value: 1, label: 'sunt aut facere' },
+					{ value: 2, label: 'qui est esse' },
+				],
 			],
-		],
-	] )( 'when given %o with path %o and none %o it returns %o', ( input, paths, none, expected ) => {
-		expect( selectOptions( input, paths, none ) ).toStrictEqual( expected );
+		] )( 'when given %o with path %o and none %o it returns %o', ( input, paths, none, expected ) => {
+			expect( selectOptions( input, paths, none ) ).toStrictEqual( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return invalid array of objects for invalid post objects array with valid property path and no custom "none" option value', () => {
-	it.each( [
-		[ [], { id: 'value' }, [ { value: '', label: '— Select —' } ] ],
-		[ {}, { id: 'value' }, [ { value: '', label: '— Select —' } ] ],
-		[ false, { id: 'value' }, [ { value: '', label: '— Select —' } ] ],
-		[ true, { id: 'value' }, [ { value: '', label: '— Select —' } ] ],
-		[ undefined, { id: 'value' }, [ { value: '', label: '— Select —' } ] ],
-		[ null, { id: 'value' }, [ { value: '', label: '— Select —' } ] ],
-	] )( 'when given %p with path %o it returns %o', ( input, paths, expected ) => {
-		expect( selectOptions( input, paths ) ).toStrictEqual( expected );
-	} );
-} );
-
-describe( 'Should return invalid array of objects with value "undefined" for valid post objects array with invalid property path and no custom "none" option value', () => {
-	it.each( [
-		[ [ { id: 1 } ], { name: 'value' }, [ { value: '', label: '— Select —' }, { value: undefined } ] ],
-		[
+	describe( 'Should return invalid array of objects with value "undefined" for valid post objects array with invalid property path and no custom "none" option value', () => {
+		it.each( [
+			[ [ { id: 1 } ], { name: 'value' }, [ { value: '', label: '— Select —' }, { value: undefined } ] ],
 			[
-				{ id: 1, title: { rendered: 'sunt aut facere' } },
-				{ id: 2, title: { rendered: 'qui est esse' } },
+				[
+					{ id: 1, title: { rendered: 'sunt aut facere' } },
+					{ id: 2, title: { rendered: 'qui est esse' } },
+				],
+				{ id: 'value', 'title,rendered': 'label' },
+				[
+					{ value: '', label: '— Select —' },
+					{ value: 1, label: undefined },
+					{ value: 2, label: undefined },
+				],
 			],
-			{ id: 'value', 'title,rendered': 'label' },
-			[
-				{ value: '', label: '— Select —' },
-				{ value: 1, label: undefined },
-				{ value: 2, label: undefined },
-			],
-		],
-	] )( 'when given %o with path %o it returns %o', ( input, paths, expected ) => {
-		expect( selectOptions( input, paths ) ).toStrictEqual( expected );
+		] )( 'when given %o with path %o it returns %o', ( input, paths, expected ) => {
+			expect( selectOptions( input, paths ) ).toStrictEqual( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return empty object for valid post objects array with empty or falseful value path and no custom "none" option value', () => {
-	it.each( [
-		[ [ { id: 1 } ], {}, [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], [], [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], undefined, [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], null, [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], false, [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], true, [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], 0, [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], -0, [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], 0n, [ { value: '', label: '— Select —' }, {} ] ],
-		[ [ { id: 1 } ], NaN, [ { value: '', label: '— Select —' }, {} ] ],
-	] )( 'when given %o with path %p it returns %o', ( input, paths, expected ) => {
-		expect( selectOptions( input, paths ) ).toStrictEqual( expected );
+	describe( 'Should accept falsey arguments for post objects array', () => {
+		const testPropertyPath = { id: 'value' };
+		const cases = map( falsey, ( value ) => [ value, testPropertyPath ] );
+		it.each( cases )( 'when given %p with path %o', ( input, value ) => {
+			expect.anything( selectOptions( input, value ) );
+		} );
+	} );
+
+	describe( 'Should accept empty arguments for post objects array', () => {
+		const testPropertyPath = { id: 'value' };
+		const cases = map( empties, ( value ) => [ value, testPropertyPath ] );
+		it.each( cases )( 'when given %p with path %o', ( input, value ) => {
+			expect.anything( selectOptions( input, value ) );
+		} );
+	} );
+
+	describe( 'Should accept falsey arguments for value path', () => {
+		const testPostObject = { id: 1 };
+		const cases = map( falsey, ( value ) => [ testPostObject, value ] );
+		it.each( cases )( 'when given %p with path %o', ( input, value ) => {
+			expect.anything( selectOptions( input, value ) );
+		} );
+	} );
+
+	describe( 'Should accept empty arguments for value path', () => {
+		const testPostObject = { id: 1 };
+		const cases = map( empties, ( value ) => [ testPostObject, value ] );
+		it.each( cases )( 'when given %p with path %o', ( input, value ) => {
+			expect.anything( selectOptions( input, value ) );
+		} );
 	} );
 } );
