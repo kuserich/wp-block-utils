9f8bbafcb4f96a143c16e3921b7c440d4fa6189f
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-07-06 14:58:49 +0200

    Use utils, update describe pattern

--- src/is-position-center/test/index.js
+++ src/is-position-center/test/index.js
@@ -1,55 +1,59 @@
 /**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { map } from 'lodash';
+
+/**
  * The function to be tested.
  *
  * @ignore
  */
 import isPositionCenter from '../';
 
-describe( 'Returns boolean primitive "true" for string "center" or "center center"', () => {
-	it.each( [
-		[ 'center', true ],
-		[ 'center center', true ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( isPositionCenter( input ) ).toBe( expected );
-	} );
-} );
+/**
+ * Set of falsey and empty values for testing.
+ *
+ * @ignore
+ */
+import { falsey, empties } from '../../utils';
 
-describe( 'Should return boolean primitive "true" for falseful value', () => {
-	it.each( [
-		[ '', true ],
-		[ null, true ],
-		[ false, true ],
-		[ undefined, true ],
-		[ 0, true ],
-		[ -0, true ],
-		[ 0n, true ],
-		[ NaN, true ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( isPositionCenter( input ) ).toBe( expected );
+describe( 'isPositionCenter', () => {
+	describe( 'Returns boolean primitive "true" for string "center" or "center center"', () => {
+		it.each( [
+			[ 'center', true ],
+			[ 'center center', true ],
+		] )( 'when given %p it returns %p', ( input, expected ) => {
+			expect( isPositionCenter( input ) ).toBe( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return boolean primitive "false" for invalid string', () => {
-	it.each( [
-		[ 'Center', false ],
-		[ 'center  center', false ],
-		[ 'center center center', false ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( isPositionCenter( input ) ).toBe( expected );
+	describe( 'Should return boolean primitive "false" for invalid string', () => {
+		it.each( [
+			[ 'Center', false ],
+			[ 'center  center', false ],
+			[ 'center center center', false ],
+		] )( 'when given %p it returns %p', ( input, expected ) => {
+			expect( isPositionCenter( input ) ).toBe( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return boolean primitive "false" for value "true"', () => {
-	it.each( [ [ true, false ] ] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( isPositionCenter( input ) ).toBe( expected );
+	it( 'Should return boolean primitive "false" for value "true"', () => {
+		expect( isPositionCenter( true ) ).toBe( false );
 	} );
-} );
 
-describe( 'Should return boolean primitive "false" for empty value', () => {
-	it.each( [
-		[ [], false ],
-		[ {}, false ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( isPositionCenter( input ) ).toBe( expected );
+	describe( 'Should accept falsey arguments', () => {
+		const cases = map( falsey, ( value ) => [ value ] );
+		it.each( cases )( 'when given %p', ( input ) => {
+			expect.anything( isPositionCenter( input ) );
+		} );
+	} );
+
+	describe( 'Should accept empty arguments', () => {
+		const cases = map( empties, ( value ) => [ value ] );
+		it.each( cases )( 'when given %p', ( input ) => {
+			expect.anything( isPositionCenter( input ) );
+		} );
 	} );
 } );
