e8bcf8c9ec2c4227eeaef4010ecc57ccc8eb7951
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-07-06 14:11:46 +0200

    Use utils, update describe pattern

--- src/deep-pick/test/index.js
+++ src/deep-pick/test/index.js
@@ -1,9 +1,23 @@
 /**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { map } from 'lodash';
+
+/**
  * The function to be tested.
  *
  * @ignore
  */
 import deepPick from '../';
+
+/**
+ * Set of falsey and empty values for testing.
+ *
+ * @ignore
+ */
+import { falsey, empties } from '../../utils';
 
 /**
  * Valid collection object.
@@ -12,41 +26,47 @@ import deepPick from '../';
  */
 const validCollection = { id: 1, title: { value: 100, label: { name: 'example' } } };
 
-describe( 'Should return key-value pairs composed of the picked paths from data collection', () => {
-	test.each( [
-		[ validCollection, [ 'id' ], { id: 1 } ],
-		[ validCollection, [ 'title.label.name' ], { title: 'example' } ],
-		[ validCollection, [ 'title.rendered' ], { title: undefined } ],
-		[ validCollection, [ 'id', 'title.label.name' ], { id: 1, title: 'example' } ],
-	] )( 'when given %p with path %p it returns %p', ( collection, path, expected ) => {
-		expect( deepPick( collection, path ) ).toStrictEqual( expected );
+describe( 'deepPick', () => {
+	describe( 'Should return key-value pairs composed of the picked paths from data collection', () => {
+		test.each( [
+			[ validCollection, [ 'id' ], { id: 1 } ],
+			[ validCollection, [ 'title.label.name' ], { title: 'example' } ],
+			[ validCollection, [ 'title.rendered' ], { title: undefined } ],
+			[ validCollection, [ 'id', 'title.label.name' ], { id: 1, title: 'example' } ],
+		] )( 'when given %p with path %p it returns %p', ( collection, path, expected ) => {
+			expect( deepPick( collection, path ) ).toStrictEqual( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return key-value pairs where the value of each key is `undefined` if the given object path cannot be resolved', () => {
-	test.each( [
-		[ {}, [ 'id' ], { id: undefined } ],
-		[ [], [ 'id' ], { id: undefined } ],
-		[ false, [ 'id' ], { id: undefined } ],
-		[ true, [ 'id' ], { id: undefined } ],
-		[ undefined, [ 'id' ], { id: undefined } ],
-		[ null, [ 'id' ], { id: undefined } ],
-		[ 0, [ 'id' ], { id: undefined } ],
-	] )( 'when given %p with path %p it returns %p', ( collection, path, expected ) => {
-		expect( deepPick( collection, path ) ).toStrictEqual( expected );
+	describe( 'Should accept falsey arguments for object collection', () => {
+		const testObjectPath = [ 'id' ];
+		const cases = map( falsey, ( value ) => [ value, testObjectPath ] );
+		it.each( cases )( 'when given %p with index %p', ( input, value ) => {
+			expect.anything( deepPick( input, value ) );
+		} );
 	} );
-} );
 
-describe( 'Should return an empty object when a non-existent path is given, including but not limited to an array, object, or falsely values', () => {
-	test.each( [
-		[ { id: 1 }, [], {} ],
-		[ { id: 1 }, {}, {} ],
-		[ { id: 1 }, undefined, {} ],
-		[ { id: 1 }, false, {} ],
-		[ { id: 1 }, true, {} ],
-		[ { id: 1 }, null, {} ],
-		[ { id: 1 }, 0, {} ],
-	] )( 'when given %p with path %p it returns %p', ( collection, path, expected ) => {
-		expect( deepPick( collection, path ) ).toStrictEqual( expected );
+	describe( 'Should accept empty arguments for object collection', () => {
+		const testObjectPath = [ 'id' ];
+		const cases = map( empties, ( value ) => [ value, testObjectPath ] );
+		it.each( cases )( 'when given %p with index %p', ( input, value ) => {
+			expect.anything( deepPick( input, value ) );
+		} );
+	} );
+
+	describe( 'Should accept falsey arguments for object path', () => {
+		const testObject = { id: 1 };
+		const cases = map( falsey, ( value ) => [ testObject, value ] );
+		it.each( cases )( 'when given %p with index %p', ( input, value ) => {
+			expect.anything( deepPick( input, value ) );
+		} );
+	} );
+
+	describe( 'Should accept empty arguments for object path', () => {
+		const testObject = { id: 1 };
+		const cases = map( empties, ( value ) => [ testObject, value ] );
+		it.each( cases )( 'when given %p with index %p', ( input, value ) => {
+			expect.anything( deepPick( input, value ) );
+		} );
 	} );
 } );
