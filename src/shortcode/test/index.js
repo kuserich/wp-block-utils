90d5a91a9e2f73cc0dcef2c4efd6a44e41e36163
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-07-06 10:39:50 +0200

    Use utils, update describes

--- src/shortcode/test/index.js
+++ src/shortcode/test/index.js
@@ -1,64 +1,75 @@
 /**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { map } from 'lodash';
+
+/**
  * The function to be tested.
  *
  * @ignore
  */
 import shortcode from '../';
 
-describe( 'Should return a valid string shortcode for valid tag name and attributes', () => {
-	it.each( [
-		[ 'test_tag', { posts: 1 }, '[test_tag posts="1"]' ],
-		[ 'Test Tag', { posts: 1 }, '[test_tag posts="1"]' ],
-		[ 'test_tag', { posts: [ 1, 2 ] }, '[test_tag posts="1,2"]' ],
-		[ 'test_tag', { posts: '1,2' }, '[test_tag posts="1,2"]' ],
-		[ 'test_tag', { camelCase: 1 }, '[test_tag camel_case="1"]' ],
-	] )( 'when given %s with attributes %o it returns %s', ( tagName, attributes, expected ) => {
-		expect( shortcode( tagName, attributes ) ).toBe( expected );
-	} );
-} );
+/**
+ * Set of falsey and empty values for testing.
+ *
+ * @ignore
+ */
+import { falsey, empties } from '../../utils';
 
-describe( 'Should return an invalid string shortcode for an invalid tag name and valid attribute', () => {
-	it.each( [
-		[ [ 'test_tag' ], { posts: 1 }, '[test_tag posts="1"]' ],
-		[ [ 'test_tag', 'another_tag' ], { posts: 1 }, '[test_tag_another_tag posts="1"]' ],
-		[ {}, { posts: 1 }, '[object_object posts="1"]' ],
-		[ false, { posts: 1 }, '[false posts="1"]' ],
-		[ true, { posts: 1 }, '[true posts="1"]' ],
-	] )( 'when given %p with attributes %o it returns %s', ( tagName, attributes, expected ) => {
-		expect( shortcode( tagName, attributes ) ).toBe( expected );
+describe( 'shortcode', () => {
+	describe( 'Should return a valid string shortcode for valid tag name and attributes', () => {
+		it.each( [
+			[ 'test_tag', { posts: 1 }, '[test_tag posts="1"]' ],
+			[ 'Test Tag', { posts: 1 }, '[test_tag posts="1"]' ],
+			[ 'test_tag', { posts: [ 1, 2 ] }, '[test_tag posts="1,2"]' ],
+			[ 'test_tag', { posts: '1,2' }, '[test_tag posts="1,2"]' ],
+			[ 'test_tag', { camelCase: 1 }, '[test_tag camel_case="1"]' ],
+		] )( 'when given %s with attributes %o it returns %s', ( tagName, attributes, expected ) => {
+			expect( shortcode( tagName, attributes ) ).toBe( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return an invalid string shortcode for an empty or falseful tag name and valid attribute', () => {
-	it.each( [
-		[ [ '' ], { posts: 1 }, '[ posts="1"]' ],
-		[ [], { posts: 1 }, '[ posts="1"]' ],
-		[ '', { posts: 1 }, '[ posts="1"]' ],
-		[ null, { posts: 1 }, '[ posts="1"]' ],
-		[ undefined, { posts: 1 }, '[ posts="1"]' ],
-		[ 0, { posts: 1 }, '[0 posts="1"]' ],
-		[ -0, { posts: 1 }, '[0 posts="1"]' ],
-		[ NaN, { posts: 1 }, '[na_n posts="1"]' ],
-	] )( 'when given %p with attributes %o it returns %s', ( tagName, attributes, expected ) => {
-		expect( shortcode( tagName, attributes ) ).toBe( expected );
+	describe( 'Should return an invalid string shortcode for an invalid tag name and valid attribute', () => {
+		it.each( [
+			[ [ 'test_tag' ], { posts: 1 }, '[test_tag posts="1"]' ],
+			[ [ 'test_tag', 'another_tag' ], { posts: 1 }, '[test_tag_another_tag posts="1"]' ],
+		] )( 'when given %p with attributes %o it returns %s', ( tagName, attributes, expected ) => {
+			expect( shortcode( tagName, attributes ) ).toBe( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return an invalid string shortcode for a valid tag name and empty or falseful attribute', () => {
-	it.each( [
-		[ 'test_tag', [ '' ], '[test_tag 0=""]' ],
-		[ 'test_tag', '', '[test_tag]' ],
-		[ 'test_tag', {}, '[test_tag]' ],
-		[ 'test_tag', [], '[test_tag]' ],
-		[ 'test_tag', false, '[test_tag]' ],
-		[ 'test_tag', true, '[test_tag]' ],
-		[ 'test_tag', undefined, '[test_tag]' ],
-		[ 'test_tag', null, '[test_tag]' ],
-		[ 'test_tag', 0, '[test_tag]' ],
-		[ 'test_tag', -0, '[test_tag]' ],
-		[ 'test_tag', 0n, '[test_tag]' ],
-		[ 'test_tag', NaN, '[test_tag]' ],
-	] )( 'when given %s with attributes %p it returns %s', ( tagName, attributes, expected ) => {
-		expect( shortcode( tagName, attributes ) ).toBe( expected );
+	describe( 'Should accept falsey arguments for the tag name', () => {
+		const testAttribute = { posts: 1 };
+		const cases = map( falsey, ( value ) => [ value, testAttribute ] );
+		it.each( cases )( 'when given %p with attributes %o', ( input, value ) => {
+			expect.anything( shortcode( input, value ) );
+		} );
+	} );
+
+	describe( 'Should accept empty arguments for the tag name', () => {
+		const testAttribute = { posts: 1 };
+		const cases = map( empties, ( value ) => [ value, testAttribute ] );
+		it.each( cases )( 'when given %p with attributes %o', ( input, value ) => {
+			expect.anything( shortcode( input, value ) );
+		} );
+	} );
+
+	describe( 'Should accept falsey arguments for the attribute', () => {
+		const testTagName = 'test_tag';
+		const cases = map( falsey, ( value ) => [ testTagName, value ] );
+		it.each( cases )( 'when given %s with attributes %p', ( input, value ) => {
+			expect.anything( shortcode( input, value ) );
+		} );
+	} );
+
+	describe( 'Should accept empty arguments for the attribute', () => {
+		const testTagName = 'test_tag';
+		const cases = map( empties, ( value ) => [ testTagName, value ] );
+		it.each( cases )( 'when given %s with attributes %p', ( input, value ) => {
+			expect.anything( shortcode( input, value ) );
+		} );
 	} );
 } );
