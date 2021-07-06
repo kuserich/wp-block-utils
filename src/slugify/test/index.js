9ae783c792d45fe17f2a77c65d1b87a76687e9a9
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-07-06 10:13:25 +0200

    Use utils, restructure describes

--- src/slugify/test/index.js
+++ src/slugify/test/index.js
@@ -1,41 +1,51 @@
 /**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { map } from 'lodash';
+
+/**
  * The function to be tested.
  *
  * @ignore
  */
 import slugify from '../';
 
-describe( 'Should return slugified version of the string given by replacing whitespaces with dashes and escaping HTML elements', () => {
-	it.each( [
-		[ 'lorem ipsum', 'lorem-ipsum' ],
-		[ 'lorem 1234', 'lorem-1234' ],
-		[ 'LOREM IPSUM', 'lorem-ipsum' ],
-		[ 'lorem $ ipsum', 'lorem-dollar-ipsum' ],
-		[ 'lorem *_+~()!?—–−:@^|&#.,;%<>{} ipsum', 'lorem-orandpercent-ipsum' ],
-		[ '<a href="https://example.com"> lorem ipsum <strong>dolor</strong> </a>', 'lorem-ipsum-dolor' ],
-		[ 'unicode is ♥', 'unicode-is-love' ],
-		[ 'unicode is ☢', 'unicode-is-☢' ], // Unsupported symbol
-	] )( 'when given %s it returns %s', ( input, expected ) => {
-		expect( slugify( input ) ).toBe( expected );
-	} );
-} );
+/**
+ * Set of falsey and empty values for testing.
+ *
+ * @ignore
+ */
+import { falsey, empties } from '../../utils';
 
-describe( 'Should return empty string when given falsely argument including but not limited to an array, object, or an empty array', () => {
-	it.each( [
-		[ '', '' ],
-		[ [ '' ], '' ],
-		[ [], '' ],
-		[ {}, '' ],
-		[ undefined, '' ],
-		[ null, '' ],
-		[ false, '' ],
-		[ true, '' ],
-		[ 1, '' ],
-		[ 0, '' ],
-		[ -0, '' ],
-		[ 0n, '' ],
-		[ NaN, '' ],
-	] )( 'when given %p it returns %s', ( input, expected ) => {
-		expect( slugify( input ) ).toBe( expected );
+describe( 'slugify', () => {
+	describe( 'Should return slugified version of the string given by replacing whitespaces with dashes and escaping HTML elements', () => {
+		it.each( [
+			[ 'lorem ipsum', 'lorem-ipsum' ],
+			[ 'lorem 1234', 'lorem-1234' ],
+			[ 'LOREM IPSUM', 'lorem-ipsum' ],
+			[ 'lorem $ ipsum', 'lorem-dollar-ipsum' ],
+			[ 'lorem *_+~()!?—–−:@^|&#.,;%<>{} ipsum', 'lorem-orandpercent-ipsum' ],
+			[ '<a href="https://example.com"> lorem ipsum <strong>dolor</strong> </a>', 'lorem-ipsum-dolor' ],
+			[ 'unicode is ♥', 'unicode-is-love' ],
+			[ 'unicode is ☢', 'unicode-is-☢' ], // Unsupported symbol
+		] )( 'when given %s it returns %s', ( input, expected ) => {
+			expect( slugify( input ) ).toBe( expected );
+		} );
+	} );
+
+	describe( 'Should accept falsey arguments', () => {
+		const cases = map( falsey, ( value ) => [ value ] );
+		it.each( cases )( 'when given %p', ( input ) => {
+			expect.anything( slugify( input ) );
+		} );
+	} );
+
+	describe( 'Should accept empty arguments', () => {
+		const cases = map( empties, ( value ) => [ value ] );
+		it.each( cases )( 'when given %p', ( input ) => {
+			expect.anything( slugify( input ) );
+		} );
 	} );
 } );
