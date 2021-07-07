36e07953efde7ff86c63846c0763a5b84f39404d
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-07-07 16:43:45 +0200

    Update test structure, include utils

--- src/stringify/test/index.js
+++ src/stringify/test/index.js
@@ -1,57 +1,54 @@
 /**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { map } from 'lodash';
+
+/**
  * The function to be tested.
  *
  * @ignore
  */
 import stringify from '../';
 
-describe( 'Should return a JSON string for a valid input object', () => {
-	it.each( [
-		[ { id: 1, title: 'sunt aut facere' }, '{"id":1,"title":"sunt aut facere"}' ],
-		[ { date: new Date( +0 ) }, '{"date":"1970-01-01T00:00:00.000Z"}' ],
-		[
-			[
-				{ id: 1, title: 'sunt aut facere' },
-				{ id: 2, title: 'qui est esse' },
-			],
-			'[{"id":1,"title":"sunt aut facere"},{"id":2,"title":"qui est esse"}]',
-		],
-	] )( 'when given %p it returns %s', ( input, expected ) => {
-		expect( stringify( input ) ).toBe( expected );
-	} );
-} );
+/**
+ * Set of falsey and empty values for testing.
+ *
+ * @ignore
+ */
+import { falsey, empties } from '../../utils';
 
-describe( 'Should return an invalid string for an valid input string', () => {
-	it.each( [ [ 'lorem ipsum', '"lorem ipsum"' ] ] )( 'when given %s it returns %s', ( input, expected ) => {
-		expect( stringify( input ) ).toBe( expected );
+describe( 'stringify', () => {
+	describe( 'Should return stringified array of objects given an array of objects with a single key/value pair', () => {
+		it.each( [
+			[ { id: 1 }, '{"id":1}' ],
+			[ [ { id: 1, product: '1885' }, { action: 'hook_name' } ], '[{"id":1,"product":"1885"},{"action":"hook_name"}]' ],
+		] )( 'when given %o it returns %s', ( input, expected ) => {
+			expect( stringify( input ) ).toBe( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return an invalid array for an valid input array', () => {
-	it.each( [ [ [ 'lorem', 'ipsum' ], '["lorem","ipsum"]' ] ] )( 'when given %p it returns %s', ( input, expected ) => {
-		expect( stringify( input ) ).toBe( expected );
+	describe( 'Should return a stringified empty object given the array input includes a key/value pair with an unsupported value including but not limited to `undefined` or `Symbol()`', () => {
+		it.each( [
+			[ { product: undefined }, '{}' ],
+			[ { product: Symbol() }, '{}' ],
+		] )( 'when given %o it returns %s', ( input, expected ) => {
+			expect( stringify( input ) ).toBe( expected );
+		} );
 	} );
-} );
 
-describe( 'Should return an invalid string for falseful values', () => {
-	it.each( [
-		[ '', '""' ],
-		[ [ '' ], '[""]' ],
-		[ [], '[]' ],
-		[ {}, '{}' ],
-		[ 0, '0' ],
-		[ -0, '0' ],
-		[ NaN, 'null' ],
-		[ false, 'false' ],
-		[ true, 'true' ],
-		[ null, 'null' ],
-	] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( stringify( input ) ).toBe( expected );
+	describe( 'Should accept falsey arguments', () => {
+		const cases = map( falsey, ( value ) => [ value ] );
+		it.each( cases )( 'when given %p', ( input ) => {
+			expect.anything( stringify( input ) );
+		} );
 	} );
-} );
 
-describe( 'Should return primitive "undefined" given value "undefined"', () => {
-	it.each( [ [ undefined, undefined ] ] )( 'when given %p it returns %p', ( input, expected ) => {
-		expect( stringify( input ) ).toBe( expected );
+	describe( 'Should accept empty arguments', () => {
+		const cases = map( empties, ( value ) => [ value ] );
+		it.each( cases )( 'when given %p', ( input ) => {
+			expect.anything( stringify( input ) );
+		} );
 	} );
 } );
