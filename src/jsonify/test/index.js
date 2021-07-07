0794bd22cc8da7deb5857e030b47c3eb2b5001c7
Adrian Firoiu <adrian.firoiu@sixa.ch>
2021-07-07 13:36:16 +0200

    Add jsonify unit test

---
+++ src/jsonify/test/index.js
@@ -0,0 +1,50 @@
+/**
+ * Utility for libraries from the `Lodash`.
+ *
+ * @ignore
+ */
+import { map } from 'lodash';
+
+/**
+ * The function to be tested.
+ *
+ * @ignore
+ */
+import jsonify from '../';
+
+/**
+ * Set of falsey and empty values for testing.
+ *
+ * @ignore
+ */
+import { falsey, empties } from '../../utils';
+
+describe( 'jsonify', () => {
+	describe( 'Should return valid JSON object for given input string', () => {
+		it.each( [
+			[
+				'[{"id":1,"title":"sunt aut facere"},{"id":2,"title":"qui est esse"}]',
+				[
+					{ id: 1, title: 'sunt aut facere' },
+					{ id: 2, title: 'qui est esse' },
+				],
+			],
+		] )( 'when given %s it returns %o', ( input, expected ) => {
+			expect( jsonify( input ) ).toStrictEqual( expected );
+		} );
+	} );
+
+	describe( 'Should accept falsey arguments', () => {
+		const cases = map( falsey, ( value ) => [ value ] );
+		it.each( cases )( 'when given %p', ( input ) => {
+			expect.anything( jsonify( input ) );
+		} );
+	} );
+
+	describe( 'Should accept empty arguments', () => {
+		const cases = map( empties, ( value ) => [ value ] );
+		it.each( cases )( 'when given %p', ( input ) => {
+			expect.anything( jsonify( input ) );
+		} );
+	} );
+} );
