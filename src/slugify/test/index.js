b16bbdb34ea02edd1aa32bd6f5b096147fbb5afa
Markus <markus.goeckeritz@sixa.ch>
2021-07-11 14:35:00 +0200

    restructured test cases

--- src/slugify/test/index.js
+++ src/slugify/test/index.js
@@ -20,16 +20,44 @@ import slugify from '../';
 import { falsey, empties } from '../../utils';
 
 describe( 'slugify', () => {
-	describe( 'Should return slugified version of the string given by replacing whitespaces with dashes and escaping HTML elements', () => {
+	it( 'Should replace whitespaces between words with dashes', () => {
+		const input = 'my slug';
+		const expected = 'my-slug';
+		expect( slugify( input ) ).toBe( expected );
+	} );
+
+	it( 'Should remove trailing and leading whitespaces', () => {
+		const input = ' my slug ';
+		const expected = 'my-slug';
+		expect( slugify( input ) ).toBe( expected );
+	} );
+
+	it( 'Should lowercase capital letters', () => {
+		const input = 'My Slug';
+		const expected = 'my-slug';
+		expect( slugify( input ) ).toBe( expected );
+	} );
+
+	describe( 'Should replace symbols', () => {
+		// These cases are only a tiny subset of all symbols that
+		// are replaced. For a list of all symbols, please refer
+		// to the test cases of the slugify NPM package.
+		// @see https://github.com/simov/slugify/blob/master/test/slugify.js
 		it.each( [
-			[ 'lorem ipsum', 'lorem-ipsum' ],
-			[ 'lorem 1234', 'lorem-1234' ],
-			[ 'LOREM IPSUM', 'lorem-ipsum' ],
-			[ 'lorem $ ipsum', 'lorem-dollar-ipsum' ],
-			[ 'lorem *_+~()!?—–−:@^|&#.,;%<>{} ipsum', 'lorem-orandpercent-ipsum' ],
-			[ '<a href="https://example.com"> lorem ipsum <strong>dolor</strong> </a>', 'lorem-ipsum-dolor' ],
+			[ 'my $ slug', 'my-dollar-slug' ],
+			[ 'my > slug', 'my-greater-slug' ],
 			[ 'unicode is ♥', 'unicode-is-love' ],
-			[ 'unicode is ☢', 'unicode-is-☢' ], // Unsupported symbol
+			[ 'unicode is ☢', 'unicode-is-☢' ],
+		] )( 'when given %s it returns %s', ( input, expected ) => {
+			expect( slugify( input ) ).toBe( expected );
+		} );
+	} );
+
+	describe( 'Should strip all HTML tags including script and style', () => {
+		it.each( [
+			[ '<a href="https://example.com">lorem ipsum<strong>dolor</strong></a>', 'lorem-ipsumdolor' ],
+			[ '<style>html { color: red; }</style>', 'html-color-red' ],
+			[ '<script>alert("hello world");</script>', 'alerthello-world' ],
 		] )( 'when given %s it returns %s', ( input, expected ) => {
 			expect( slugify( input ) ).toBe( expected );
 		} );
