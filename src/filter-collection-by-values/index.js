c7cbd458c1fd6f7c72a95854e4bd358c697476c5
Markus <markus.goeckeritz@sixa.ch>
2022-01-06 00:07:06 +0100

    added function filterCollectionByValues

---
+++ src/filter-collection-by-values/index.js
@@ -0,0 +1,36 @@
+/**
+ * Utility functions from `Lodash`.
+ *
+ * @see    https://lodash.com/docs
+ * @ignore
+ */
+import { forEach, find } from 'lodash';
+
+/**
+ * Iterates over elements of `values` and returns the matching entry
+ * from ´collection´, if there is a match.
+ *
+ * @function
+ * @since     1.2.0
+ * @param     {Array}     values        The values to search for.
+ * @param     {Array}     collection    The collection to search elements in.
+ * @param     {string}    field         The field to search for. Defaults to 'id'.
+ * @return    {Array}                   All matching entries in collection.
+ * @example
+ *
+ * filterCollectionByValues( [ 1, 3 ], [ { id: 1, content: {} }, { id: 2, content: {} }, { id: 3, content: {} } ] );
+ *
+ * // => Array [ { id: 1, content: {} }, { id: 3, content: {} } ]
+ */
+const filterCollectionByValues = ( values, collection, field = 'id' ) => {
+	const existing = [];
+	forEach( values, ( value ) => {
+		const match = find( collection, [ field, value ] );
+		if ( Boolean( match ) ) {
+			existing.push( match );
+		}
+	} );
+	return existing;
+};
+
+export default filterCollectionByValues;
